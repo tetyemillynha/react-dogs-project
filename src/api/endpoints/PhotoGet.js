b7e60692e05146f5f49700e712423239ac408d8a
Stefhany Oliveira <stefhany.oliveira@contabilizei.com.br>
2021-09-27 20:09:29 -0300

    Criaçao do header

---
+++ src/api/endpoints/PhotoGet.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const PhotoGet = () => {
+  const [img, setImg] = React.useState('');
+  const [id, setId] = React.useState('');
+
+  function handleSubmit(event){
+    event.preventDefault();
+    fetch(`https://dogsapi.origamid.dev/json/api/photo/${id}`).then( response => {
+        console.log(response);
+        return response.json();
+      }).then(json => {
+        console.log(json);
+        setImg(json.photo.src);
+        return json;
+    })
+
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={id} onChange={({ target }) => setId(target.value)}/>
+        <button>Enviar</button>
+      </form>
+      { img }
+    </div>
+  )
+}
+
+export default PhotoGet
